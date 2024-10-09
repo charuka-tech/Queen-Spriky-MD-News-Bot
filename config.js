000efc657ea402b3c24b6c4773030b1ff1faba99
charuka-tech <168742032+charuka-tech@users.noreply.github.com>
2024-10-09 15:43:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',    // Enter Your MongoDB URL
-    Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
-    BotNumber: process.env.BotNumber || '94773366833'    // Enter Your Bot Number
+    SESSION_ID: process.env.SESSION_ID || 'ab5STSaQ#kBuTelkwEU2kwYFu3nTD0wxstiCINRRRzr7se_cqJpk', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net',    // Enter Your MongoDB URL
+    Owner: process.env.OwnerNumber || '94775228948',    // Enter Your Owner Number
+    BotNumber: process.env.BotNumber || '94775228949'    // Enter Your Bot Number
 };
